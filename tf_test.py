081f2db412266fffa13ea5194943a66ca63a173f
Alex Rpd <rapidisalex@gmail.com>
2020-10-22 19:18:56 +0100

    cleaning repo

--- tf_test.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-
-if __name__ == '__main__':
-    print("TF version: ", tf.__version__)
-    print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
